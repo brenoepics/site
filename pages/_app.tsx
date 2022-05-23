cb53906e351d49269135ba2a33febf30b074acd2
Guilherme Puida Moreira <guilhermepmoreira00@gmail.com>
2022-05-23 17:11:38 -0300

    Added mantine component library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { MantineProvider } from "@mantine/core";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = (props: AppProps) => {
+  const { Component, pageProps } = props;
 
-export default MyApp
+  return (
+    <>
+      <Head>
+        <title>Sophia</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+};
+
+export default App;
