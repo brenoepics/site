50ae15be0e343ce56af07b0628066a095deafc18
Guilherme Puida Moreira <guilhermepmoreira00@gmail.com>
2022-05-23 17:17:55 -0300

    Formatted files with prettier

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
-import { createGetInitialProps } from "@mantine/next";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { createGetInitialProps } from '@mantine/next';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-const getInitialProps = createGetInitialProps()
+const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
@@ -15,6 +15,6 @@ export default class _Document extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
