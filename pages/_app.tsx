a8ed0308d802ebab234e3b947fba8cd4dbbe194a
Luca Candelot <lups0x01@gmail.com>
2022-06-19 02:38:08 -0300

    tfirst styling of homePage.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { MantineProvider } from '@mantine/core';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import './css/style.css';
 
 const App = (props: AppProps) => {
   const { Component, pageProps } = props;
@@ -12,7 +13,13 @@ const App = (props: AppProps) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      <MantineProvider withGlobalStyles withNormalizeCSS>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+        }}
+      >
         <Component {...pageProps} />
       </MantineProvider>
     </>
